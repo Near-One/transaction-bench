f4910c32bbe880c46bd19f09a224a9bc3f4d46de
Andrei <122784628+andrei-near@users.noreply.github.com>
2025-08-07 15:33:06 +0300

    5s

--- src/config.rs
+++ src/config.rs
@@ -65,7 +65,7 @@ pub struct Opts {
     #[clap(env, short, long, value_parser = humantime::parse_duration, default_value = "15m")]
     pub period: std::time::Duration,
     /// Time delay between each intervalgroup of transactions
-    #[clap(env, short, long, value_parser = humantime::parse_duration, default_value = "3s")]
+    #[clap(env, short, long, value_parser = humantime::parse_duration, default_value = "5s")]
     pub group_delay: std::time::Duration,
     /// Override intervals for specific transaction types (JSON format: {"MpcSign": "5m", "Swap": "10m"})
     #[clap(env, long, value_parser = parse_interval_overwrite)]
